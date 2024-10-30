8b0d37967cb042a2c2c0c64ae4f65235cae77262
kamijoucen <kamijoucen@foxmail.com>
2024-10-31 00:52:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# GengineX
+
+本项目基于 bilibili Gengine 二次开发
+
+原仓库地址：https://github.com/bilibili/gengine
+
---
+++ internal/iparser/gengine_paser_vistior.go
@@ -0,0 +1,11 @@
+package iparser
+
+import parser "github.com/kamijoucen/genginex/internal/iantlr/alr"
+
+type GengineParserVisitor struct {
+	parser.BasegengineVisitor
+}
+
+func NewGengineParserVisitor() *GengineParserVisitor {
+	return &GengineParserVisitor{}
+}
